3e0eafe1674eab8aa0972b1cad0ad1812c5fd7a2
RodikYermak <rodion.yermakov.sd@gmail.com>
2019-10-10 17:59:17 -0700

    static API connection
    first api connection just to check loading and basic arguments

--- src/App.js
+++ src/App.js
@@ -11,17 +11,74 @@ import {
   faInstagram
 } from '@fortawesome/free-brands-svg-icons';
 
-function App() {
-  return (
-    <div>
-      <SearchForm />
-      <Rectangler iconname={faFacebook} area='my area' years='78.20' />
-      <Rectangler iconname={faTwitter} area='san diego county' years='81.43' />
-      <Rectangler iconname={faYoutube} area='california' years='80.90' />
-      <Rectangler iconname={faInstagram} area='united states' years='78.60' />
-      <SocialFollow />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    items: [],
+    error: null,
+    isLoaded: false
+  };
+
+  componentDidMount() {
+    const url = 'https://data.cdc.gov/resource/5h56-n989.json';
+    fetch(url)
+      .then(res => res.json())
+      .then(
+        result => {
+          console.log(result[0]);
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+          <SearchForm />
+          {/* <Rectangler iconname={faFacebook} area='my area' years='78.20' /> */}
+          <Rectangler
+            iconname={faFacebook}
+            area={items[0].full_ct_num}
+            // years='78.20'
+            years={items[0].le}
+          />
+
+          <Rectangler
+            iconname={faTwitter}
+            // area='san diego county'
+            area={items[0].county_name}
+            years='81.43'
+          />
+          {/* <Rectangler iconname={faYoutube} area='california' years='80.90' /> */}
+          <Rectangler
+            iconname={faYoutube}
+            area={items[0].state_name}
+            years='80.90'
+          />
+          <Rectangler
+            iconname={faInstagram}
+            area='united states'
+            years='78.60'
+          />
+          <SocialFollow />
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
